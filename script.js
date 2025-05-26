bb25449abadba85c5e0d64762c6582a8bc53de39
I-amPrashant <airmax50cent@gmail.com>
2025-05-26 12:25:26 +0545

    header and hero section complete with animation

--- script.js
+++ script.js
@@ -8,4 +8,21 @@ document.addEventListener('DOMContentLoaded', ()=>{
     document.querySelector('#sidebar-close').addEventListener('click', () => {
         document.getElementsByTagName('nav')[0].classList.add('translate-x-full')
     })
+
+    //scroll view and hide animation 
+    const header=document.getElementById('header');
+    let lastScroll=window.scrollY;
+    window.addEventListener('scroll', () => {
+        console.log('i ran ')
+        const currentScroll=window.scrollY;
+        if(currentScroll > lastScroll && window.scrollY > 100){
+            header.classList.add('scrolled', 'scrolled-to-top');
+        }else if(scrollY!==0){
+            header.classList.remove('scrolled');
+        }
+        if(scrollY === 0){
+            header.classList.remove('scrolled-to-top');
+        }
+        lastScroll=currentScroll;
+    })
 })
