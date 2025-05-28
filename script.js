5d3399f8a1411250b92dd2ab41bbd7097f4b579a
I-amPrashant <airmax50cent@gmail.com>
2025-05-28 14:06:35 +0545

    add shots in nepal and gallery section

--- script.js
+++ script.js
@@ -1,79 +1,63 @@
-import {data} from './demodata.js';
+document.addEventListener("DOMContentLoaded", () => {
+  const navbarWrapper = document.getElementById("navbar-item-wrapper");
+  const hamburger = document.getElementById("hamburger");
 
-document.addEventListener('DOMContentLoaded', ()=>{
+  hamburger.addEventListener("click", () => {
+    navbarWrapper.classList.toggle("h-0");
+    navbarWrapper.classList.toggle("h-[280px]");
+  });
+
+  document.documentElement.addEventListener("click", (e) => {
+    if (!e.target.closest("#hamburger")) {
+      navbarWrapper.classList.add("h-0");
+      navbarWrapper.classList.remove("h-[280px]");
+    }
+    //or
+    // !e.target.closest('#hamburger')// this will return true or false as per the element is sibling or child of #hamburger
+  });
+
+    //shots in nepal aniamtion js
+    function animation() {
+      const scrollerInner = Array.from(document.querySelectorAll(".scroller_inner"));
+      scrollerInner.forEach((scrollerItem) => {
+          const scrollItem = Array.from(scrollerItem.children);
     
-     const navbarWrapper = document.getElementById("navbar-item-wrapper");
-    const hamburger = document.getElementById("hamburger");
+          scrollItem.forEach((item) => {
+            const clonedNode = item.cloneNode(true);
+            scrollerItem.appendChild(clonedNode);
+          });
+      })
+    }
+    animation();
+  });
 
-    hamburger.addEventListener("click", () => {
-        navbarWrapper.classList.toggle("h-0");
-        navbarWrapper.classList.toggle("h-[280px]");
-    });
+  // const header=document.getElementById('header');
 
-    document.documentElement.addEventListener("click", (e) =>{
-        if (!e.target.closest("#hamburger")) {
-            navbarWrapper.classList.add("h-0");
-            navbarWrapper.classList.remove("h-[280px]");
-        }
-        //or
-        // !e.target.closest('#hamburger')// this will return true or false as per the element is sibling or child of #hamburger
-    });
-    
-    const achievementDataButton=Array.from(document.getElementById('date-wrapper').children)
+  //force to send user to top of the page onload
+  // scrollY!==0?header.classList.add('scrolled'):null;
+  // window.scrollTo(0,0);
 
-    //default content display 
-    data.forEach(data=>{
-        if(data.id===document.getElementsByClassName('active-date')[0].id){
-            document.getElementById('achievement-content').textContent=data.description
-            document.getElementById('achievement-date').textContent=data.date
-        }
-    })
-    achievementDataButton.forEach(item=>{
-        item.addEventListener('click',()=>{
-            achievementDataButton.forEach(item=>{
-                item.classList.remove('active-date')
-            })
-            item.classList.add('active-date')
+  //sidebar toggle script
+  // document.querySelector('#hamburger').addEventListener('click', () => {
+  //     document.getElementsByTagName('nav')[0].classList.remove('translate-x-full')
+  // })
 
-            data.forEach(data=>{
-                if(data.id===item.id){
-                    document.getElementById('achievement-content').textContent=data.description
-                    document.getElementById('achievement-date').textContent=data.date
-                }
-            })
-           
-        })
-    });
-    
-    
-    // const header=document.getElementById('header');
+  // document.querySelector('#sidebar-close').addEventListener('click', () => {
+  //     document.getElementsByTagName('nav')[0].classList.add('translate-x-full')
+  // })
 
-    //force to send user to top of the page onload
-    // scrollY!==0?header.classList.add('scrolled'):null;
-    // window.scrollTo(0,0);
+  //scroll view and hide animation
+  // let lastScroll=window.scrollY;
+  // window.addEventListener('scroll', () => {
+  //     const currentScroll=window.scrollY;
+  //     if(currentScroll > lastScroll && window.scrollY > 100){
+  //         header.classList.add('scrolled', 'scrolled-to-top');
+  //     }else if(scrollY!==0){
+  //         header.classList.remove('scrolled');
+  //     }
+  //     if(scrollY === 0){
+  //         header.classList.remove('scrolled-to-top');
+  //     }
+  //     lastScroll=currentScroll;
+  // })
 
-
-    //sidebar toggle script 
-    // document.querySelector('#hamburger').addEventListener('click', () => {
-    //     document.getElementsByTagName('nav')[0].classList.remove('translate-x-full')
-    // })
-
-    // document.querySelector('#sidebar-close').addEventListener('click', () => {
-    //     document.getElementsByTagName('nav')[0].classList.add('translate-x-full')
-    // })
-
-    //scroll view and hide animation 
-    // let lastScroll=window.scrollY;
-    // window.addEventListener('scroll', () => {
-    //     const currentScroll=window.scrollY;
-    //     if(currentScroll > lastScroll && window.scrollY > 100){
-    //         header.classList.add('scrolled', 'scrolled-to-top');
-    //     }else if(scrollY!==0){
-    //         header.classList.remove('scrolled');
-    //     }
-    //     if(scrollY === 0){
-    //         header.classList.remove('scrolled-to-top');
-    //     }
-    //     lastScroll=currentScroll;
-    // })
-})
